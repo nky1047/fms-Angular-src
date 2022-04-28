7913f49b4640451c62f74b821bc33a4201f1d7d2
NITIN KUMAR YADAV <nky1047@gmail.com>
2022-04-28 19:10:17 +0530

    Update app-routing.module.ts
    Module addition to app-routing.module.ts

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
+
 import { Routes, RouterModule } from '@angular/router';
+
 import { AddAirportComponent } from './add-airport/add-airport.component';
 import { ViewAirportlistComponent } from './view-airportlist/view-airportlist.component';
 import { ViewAirportComponent } from './view-airport/view-airport.component';
+
 import { HomeComponent } from './home/home.component';
+
 const routes: Routes = [
 
   { path: '', redirectTo: 'home', pathMatch: 'full' },
